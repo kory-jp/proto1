9fb578652a8b9d06d2e10ec13838c7db74daef0e
kory-job <catcher.intherye.rk@gmail.com>
2021-05-26 15:46:36 +0900

    コントローラー、ルーター作成

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :user do
+  namespace :api do
+    namespace :v1 do
+      namespace :user do
+        root "top#index"
+      end
+
+      namespace :admin do
+        root"top#index"
+      end
+    end
   end
 end
